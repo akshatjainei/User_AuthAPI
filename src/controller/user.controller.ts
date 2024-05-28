3ef1e5c1ebc8250f42d8f0d2c1f24cf657da3b0b
akshatjainei <jainakshat447@gmail.com>
2024-05-28 23:40:56 +0530

    validator and service

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,17 @@
 import { Request , Response } from "express";
+import { CreateUserInput } from "../schema/user.schema";
+import { createUser } from "../service/user.service";
+import log from "../utils/logger";
 
-export async function createUserHandler(req : Request, res : Response) {
-    
+export async function createUserHandler(req : Request<{},{},CreateUserInput>, res : Response) {
+    const body = req.body
+    try {
+        const user = await createUser(req.body)
+        return res.send("User created Successfuly")
+    } catch (error) {
+        if(error === 11000){
+            return res.status(409).send('Account already exists')
+        }
+        return res.status(500).send(error)
+    }
 }
